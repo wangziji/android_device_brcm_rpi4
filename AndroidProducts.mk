msft-mirror-aosp/device.linaro.hikey

Fix lunch choices for git_main
We add "trunk_staging-" for all COMMON_LUNCH_CHOICES so they are
valid in the trunk stable setup.

Bug: 307738446
Test: lunch
Change-Id: Iba97af30d474696210b68f5b35604bf04b953896

AndroidProducts.mk
@@ -18,8 +18,8 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/hikey960_tv.mk
 
 COMMON_LUNCH_CHOICES := \
-    hikey-userdebug \
-    hikey64_only-userdebug \
-    hikey_tv-userdebug \
-    hikey960-userdebug \
-    hikey960_tv-userdebug
+    hikey-trunk_staging-userdebug \
+    hikey64_only-trunk_staging-userdebug \
+    hikey_tv-trunk_staging-userdebug \
+    hikey960-trunk_staging-userdebug \
+    hikey960_tv-trunk_staging-userdebug

wangziji/android_device_brcm_rpi4

AndroidProducts.mk
@@ -10,6 +10,6 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/aosp_rpi4_tv.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_rpi4-userdebug \
-    aosp_rpi4_car-userdebug \
-    aosp_rpi4_tv-userdebug
+    aosp_rpi4-trunk_staging-userdebug \
+    aosp_rpi4_car-trunk_staging-userdebug \
+    aosp_rpi4_tv-trunk_staging-userdebug
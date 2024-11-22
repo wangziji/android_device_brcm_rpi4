fe5a39b0677119fe7da2980b074ac2622eecac79
Konsta <konsta09@gmail.com>
2024-11-22 16:25:11 +0200

    bluetooth: switch to aidl hal

--- device.mk
+++ device.mk
@@ -74,7 +74,7 @@ PRODUCT_COPY_FILES += \
 
 # Bluetooth
 PRODUCT_PACKAGES += \
-    android.hardware.bluetooth@1.1-service.btlinux
+    android.hardware.bluetooth-service.rpi
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.bluetooth.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth.xml \

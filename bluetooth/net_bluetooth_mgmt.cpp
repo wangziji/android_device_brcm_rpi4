21a6d545b27e6e0656458f96bbf5a0fd08325729
Konsta <konsta09@gmail.com>
2024-11-22 16:22:10 +0200

    bluetooth: rename aidl hal for rpi

--- bluetooth/net_bluetooth_mgmt.cpp
+++ bluetooth/net_bluetooth_mgmt.cpp
@@ -1,5 +1,6 @@
 /*
- * Copyright 2022 The Android Open Source Project
+ * Copyright (C) 2022 The Android Open Source Project
+ * Copyright (C) 2024 KonstaKANG
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,7 +15,7 @@
  * limitations under the License.
  */
 
-#define LOG_TAG "android.hardware.bluetooth.service.default"
+#define LOG_TAG "android.hardware.bluetooth.service.rpi"
 
 #include "net_bluetooth_mgmt.h"
 
